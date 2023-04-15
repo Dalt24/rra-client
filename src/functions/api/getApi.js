edeea695d7f691882d4eef30372d37f6a8ef96c1
Dalt24 <dcwright4@crimson.ua.edu>
2023-04-15 15:31:36 -0500

    redirected api url

--- src/functions/api/getApi.js
+++ src/functions/api/getApi.js
@@ -1,4 +1,4 @@
 export function getApiBaseUrl() {
+    if (window.location.hostname === "localhost") return 'https://localhost:7202'
     return 'https://mis-rra-api-1.herokuapp.com'
-    // return 'http://localhost:3000'
 }
